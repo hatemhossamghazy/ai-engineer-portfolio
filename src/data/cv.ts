ba7e927710cd30d34d80869b95153de92941669f
Hatem Hossam Ghazy <hatm51477@gmail.com>
2026-09-10 13:25:42 +0300

    Update profile image path in cv.ts

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -22,7 +22,7 @@ export const profile: Profile = {
   whatsapp: "01018170410",
   github: "https://github.com/hatemhossamghazy",
   linkedin: "https://www.linkedin.com/in/hatem-hossam-ghazy-06b112345/",
-  image: "/WhatsApp Image 2026-03-25 at 11.45.17 PM.jpeg",
+  image: "/profile.jpeg",
 };
 
 export const about = {
