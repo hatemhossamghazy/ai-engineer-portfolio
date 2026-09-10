cef5f0ceea544cb6cf0300e944dca1b29572322c
Hatem Hossam Ghazy <hatm51477@gmail.com>
2026-09-10 15:01:19 +0300

    Configure base path for GitHub Pages
    Set base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,8 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  vite: {
+    // تحديد مسار الرابط الخاص بمستودع GitHub Pages
+    base: "/hatem-hossam-ghazy-portfolio/",
+  },
 });
