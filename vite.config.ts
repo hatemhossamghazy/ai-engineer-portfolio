95c0510d859c6e281b6167b4807a12e4d93b32ec
Hatem Hossam Ghazy <hatm51477@gmail.com>
2026-09-10 16:02:56 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     server: { entry: "server" },
   },
   vite: {
-    base: "/hatem-ghazy-portfolio-/",
+    base: "./", // استخدام مسار نسبي يجعل المتصفح يجد ملفات الـ JS والـ CSS في أي مكان
   },
 });
